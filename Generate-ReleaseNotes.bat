3d0b092028e510a9bd337961547c3d10805983a7
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-06-15 12:04:46 +0200

    0.0.1

--- Generate-ReleaseNotes.bat
+++ Generate-ReleaseNotes.bat
@@ -1,6 +1,6 @@
 rem https://github.com/StefH/GitHubReleaseNotes
 
-SET version=0.0.1-preview-00
+SET version=0.0.1
 
 GitHubReleaseNotes --output ReleaseNotes.md --skip-empty-releases --exclude-labels question invalid documentation --version %version% --token %GH_TOKEN%
 
---
+++ ReleaseNotes.md
@@ -0,0 +1,3 @@
+# 0.0.1 (15 June 2025)
+- [#1](https://github.com/StefH/n8n.SDK/pull/1) - Initial code [enhancement] contributed by [StefH](https://github.com/StefH)
+
